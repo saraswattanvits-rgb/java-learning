f0cf19887950c809a5e4ea5dbfce093b3163f75d
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-09-27 21:04:01 +0530

    updates

---
+++ src/main/java/Test1.java
@@ -0,0 +1,42 @@
+public class Test1{
+
+}
+class A {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+class B{
+    public static void main(String[] args) {
+        System.out.println("Hello B");
+    }
+}
+
+class C{
+
+}
+
+//  NOoooooooo
+// One file can have only 1 public class
+// only that class can be public whose name is same as file name
+
+
+// Java is blocking language
+// Free form
+// naming Conventions
+// hugarian
+// mynameiskhan
+
+// MyNameIsKhan - hungarian
+// myNameIsKhan - camel case
+// Java file first letter - [A-Za-z$_]
+// 0Test - COmpile
+
+// Test  - yes
+// !Test - no
+// 1Test - no
+// Test1 - yes
+// Test% - No
+// $Test - Yes
+
+
