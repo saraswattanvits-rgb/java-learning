98fcfc48bb4f1b961155740f67c8ab3615139375
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-09-27 21:22:06 +0530

    my changes

---
+++ src/main/java/VariablesDemo.java
@@ -0,0 +1,21 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        // variables  = vary ( can change ) + able
+
+        int a = 10;
+        int b = 7;
+
+        // swap that values
+        a = a * b;         // a = 70, b = 7
+        b = a / b;         // a  = 70, b = 10
+        a = a / b;         // a  = 7, b = 10
+
+        // a = 7, b = 10
+
+        // JDK , JRE and JVM diff
+        // Obfuscation  ?
+
+
+        // Datatype
+    }
+}
