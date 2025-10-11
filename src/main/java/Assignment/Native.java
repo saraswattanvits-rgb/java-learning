de7c117e8808d0b81dc1d1174bf471b27c14a953
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-10-12 00:01:43 +0530

    Assignment

---
+++ src/main/java/Assignment/Native.java
@@ -0,0 +1,11 @@
+package Assignment;
+
+public class Native {
+    public static void main(String[] args) {
+        char c1 = '\u0924';
+        char c2 = '\u0928';
+        char c3 = '\u0935';
+        char c4 = '\u0940';
+        System.out.println(c1 +"" +c2 +"" +c3 +"" +c4);
+    }
+}
