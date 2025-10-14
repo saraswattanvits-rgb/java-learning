39b0c278f87a0be6737ab0aa01601f2ab24e4651
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-10-14 17:54:38 +0530

    Create MinutesConversion.java

---
+++ src/main/java/Assignment/MinutesConversion.java
@@ -0,0 +1,21 @@
+package Assignment;
+
+
+import java.util.Scanner;
+
+public class MinutesConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter minutes:");
+        long total_minutes = sc.nextLong();
+        long minutesInYear = 365 * 60 * 24 ;
+        long remaining_minutes = total_minutes%minutesInYear;
+        long minutesInDay = 60 * 24 ;
+        long years = total_minutes/minutesInYear;
+        long days = remaining_minutes/minutesInDay;
+        System.out.println(years +" Years");
+        System.out.println((days + " days"));
+
+
+    }
+}
