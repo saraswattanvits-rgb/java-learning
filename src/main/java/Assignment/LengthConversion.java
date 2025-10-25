924a4a2a5a9080be76523b76f1db24775e773dfd
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-10-26 00:13:44 +0530

    Update LengthConversion.java

--- src/main/java/Assignment/LengthConversion.java
+++ src/main/java/Assignment/LengthConversion.java
@@ -5,12 +5,21 @@ import java.util.Scanner;
 public class LengthConversion {
     public static void main(String[] args) {
         Scanner s1 =  new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        double length = s1.nextDouble();
-        double meters = length*0.0254 ;
-        double rounded = Math.round(meters*100.0)/100.0;
+        System.out.println("Enter the length in inches:");
+        double input = 0.0;
+        String inches =  s1.nextLine().trim();
+        while(true) {
+            try {
+                input = Double.parseDouble(inches);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input.");
+                break;
+            }
+        }
+        double meters = input * 0.0254;
+        double rounded = Math.round(meters * 100.0) / 100.0;
         System.out.println(rounded);
-
-
+        }
     }
-}
+
