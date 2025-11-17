8a086021927a78d47cefb02d7881869986770908
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-11-17 17:24:04 +0530

    Create FibonacciSeries.java

---
+++ src/main/java/Assignment/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of series:");
+        int length = sc.nextInt();
+
+        int a = 0;
+        int b = 1;
+
+        for(int i =1 ; i <= length; i++){
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
