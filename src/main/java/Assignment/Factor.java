961734912b646dd01304201c7205478bf2623794
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-11-17 17:30:40 +0530

    Create Factor.java

---
+++ src/main/java/Assignment/Factor.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Factor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        for(int i = 1 ; i <= num; i++){
+            if(num%i==0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
