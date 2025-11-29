b3d5d5dfc39f4abb631b62e553a40fb11d1ca36b
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-11-29 22:52:11 +0530

    Update MinutesConversion.java

--- src/main/java/Assignment/MinutesConversion.java
+++ src/main/java/Assignment/MinutesConversion.java
@@ -1,20 +1,25 @@
 package Assignment;
 
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MinutesConversion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter minutes:");
-        long total_minutes = sc.nextLong();
-        long minutesInYear = 365 * 60 * 24 ;
-        long minutesInDay = 60 * 24 ;
-        long years = total_minutes/minutesInYear;
-        long days = total_minutes/minutesInDay;
-        System.out.println(years +" Years");
-        System.out.println((days + " days"));
-
+        try {
+            System.out.println("Enter minutes:");
+            long total_minutes = sc.nextLong();
+            long minutesInYear = 365 * 60 * 24;
+            long minutesInDay = 60 * 24;
+            long years = total_minutes / minutesInYear;
+            long days = total_minutes / minutesInDay;
+            System.out.println(years + " Years");
+            System.out.println((days + " days"));
+        }
+        catch (InputMismatchException e){
+            System.out.println("Invalid input! Please enter valid values.");
+        }
 
     }
 }
