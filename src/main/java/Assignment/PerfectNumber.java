fca6ef7f195ea03faa7cfaef13819dd7d00ac936
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-11-29 22:49:15 +0530

    Update PerfectNumber.java

--- src/main/java/Assignment/PerfectNumber.java
+++ src/main/java/Assignment/PerfectNumber.java
@@ -1,23 +1,29 @@
 package Assignment;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PerfectNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        try{
         System.out.println("Enter a number: ");
-        int number = sc.nextInt();
+        double number = sc.nextDouble();
         if(isPerfectNumber(number)) {
             System.out.println(number + " is a perfect number.");
         }
         else{
             System.out.println(number+ " is not a perfect number.");
+        }}
+        catch (InputMismatchException e){
+            System.out.println("Invalid input! Please enter a valid number.");
         }
+
 
     }
 
-    static  boolean isPerfectNumber(int num){
-        int sum = 0;
+    static  boolean isPerfectNumber(double num){
+        double sum = 0;
         for(int i =1; i<num; i++){
             if(num%i==0){
                 sum =  sum + i;
