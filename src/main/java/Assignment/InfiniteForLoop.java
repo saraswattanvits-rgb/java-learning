ccd3f844a196ca619e270e0a4483e9f99d2cebcd
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-11-30 00:03:27 +0530

    Create InfiniteForLoop.java

---
+++ src/main/java/Assignment/InfiniteForLoop.java
@@ -0,0 +1,13 @@
+package Assignment;
+
+public class InfiniteForLoop {
+    public static void main(String[] args) {
+        //for(;;){
+        //    System.out.println("Hello");
+        //}
+
+        for(; true;){
+            System.out.println("This is an infinite for loop");
+        }
+    }
+}
