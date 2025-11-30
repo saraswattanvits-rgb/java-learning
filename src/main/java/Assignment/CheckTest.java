296ea3f548acc3b9e905c4c079da2959b1e95eac
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-11-30 17:07:47 +0530

    Create CheckTest.java

---
+++ src/main/java/Assignment/CheckTest.java
@@ -0,0 +1,80 @@
+package Assignment;
+
+public class CheckTest {
+    public static void main(String[] args) {
+
+    }
+    public class Human{
+        String name;
+        int age;
+        int height;
+        float weight;
+    }
+
+    public class MobilePhone{
+        String brand;
+        int memory;
+        int length;
+        String color;
+    }
+
+    public class Desktop{
+        String brand;
+        int width;
+        String color;
+    }
+
+    public class College{
+        String name;
+        String city;
+    }
+
+    public class TestSuite{
+        String name;
+        String testype;
+    }
+
+    public class TestCase{
+        String name;
+        String app_name;
+    }
+
+
+    public class TestStep{
+        int num;
+        String name;
+        int count;
+
+    }
+
+    public class Bag{
+        String color;
+        int length;
+        int number_of_zip;
+        String brand;
+    }
+
+    public class Shoes{
+        String brand;
+        String color;
+        int size;
+    }
+
+    public class Pen{
+        String brand;
+        String color;
+    }
+
+    public class AC{
+        String brand;
+        double weight;
+        String rating;
+    }
+
+    public class Mattress{
+        String color;
+        int width;
+        int height;
+        String brand;
+    }
+}
