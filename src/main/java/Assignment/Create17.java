553583c68c3395cf4647ac4c08a2b36f969f3558
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-11-30 16:56:00 +0530

    Create Create17.java

---
+++ src/main/java/Assignment/Create17.java
@@ -0,0 +1,15 @@
+package Assignment;
+
+public class Create17 {
+    public static void main(String[] args) {
+        int n1 = 1;
+        int n2 = 2;
+        int n3 = 6;
+        int n4 = 7;
+
+        int result =  (n3-n1) * n2 + n4;
+
+        System.out.println("Expression: (6-1) * 2 + 7 ");
+        System.out.println("Result : " +result);
+    }
+}
