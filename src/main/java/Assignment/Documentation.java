ef64fa4b61b69de12a7fcaf460d6a29d7dfea08c
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-12-02 16:11:41 +0530

    Create Documentation.java

---
+++ src/main/java/Assignment/Documentation.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+public class Documentation {
+    public static void main(String[] args) {
+        commentMethod(3,4);
+
+    }
+
+    static void commentMethod(int y, int n){
+/*This method is to learn documentation comment in java.</BR> I am writing this line for HTML presentation.*/
+
+
+        }
+}
