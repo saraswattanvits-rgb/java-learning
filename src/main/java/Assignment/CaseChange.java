d84909d4374156fca4ee431960bf41790b3ad51d
saraswattanvits-rgb <saraswat.tanvi.ts@gmail.com>
2025-12-04 00:12:22 +0530

    Create CaseChange.java

---
+++ src/main/java/Assignment/CaseChange.java
@@ -0,0 +1,25 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class CaseChange {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a sentence in lowercase: ");
+        String str = sc.nextLine();
+
+        String[] words = str.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            if (word.length() > 0) {
+
+                String upperCase = word.substring(0, 1).toUpperCase() + word.substring(1);
+                result.append(upperCase).append(" ");
+            }
+        }
+
+        System.out.println("Converted String: " + result.toString().trim());
+    }
+}
